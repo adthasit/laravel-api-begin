5d5c8e0a6d60baf054032ba6026388086122f365
Adthasid <AdthasidSuksomthawisap@Adthasids-MacBook-Pro.local>
2017-04-27 11:16:57 +0700

    Search

--- app/Http/Controllers/CarController.php
+++ app/Http/Controllers/CarController.php
@@ -10,10 +10,21 @@ use Illuminate\Support\Facades\DB;
 
 class CarController extends Controller
 {
-    public function index() {
-        $cars = Car::all();
+    //GET ALL
+    public function index(Request $request) {
+        //get ?search=search as request if exists, else empty string (")
+        $search = $request->query()?$request->query()['search']:"";
 
-        return $cars;
+        //return result like brand, gene, year and description
+        return $cars = Car::where('brand', 'like', $search.'%')
+            ->orWhere('gene', 'like', $search.'%')
+            ->orWhere('year', 'like', $search.'%')
+            ->orWhere('description', 'like', $search.'%')
+            ->get();
+
+        // $cars = Car::all();
+
+        // return $cars;
 
     }
 
